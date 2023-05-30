209ac718f9afdeccac6875ab25b1ecac5e1a2ff2
Munodawafa Nhatiso <nhatiso@yahoo.com>
2023-05-30 10:15:35 +0100

    Modifies the class name and get_user_function

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ### Computer Vision RPS
 
 Rock-Paper-Scissors is a game in which each player simultaneously shows one of three hand signals representing rock, paper, or scissors. Rock beats scissors. Scissors beats paper. Paper beats rock. The player who shows the first option that beats the other player's option wins. This is an implementation of an interactive Rock-Paper-Scissors game, in which the user can play with the computer using the camera.
+
+## Milestone 2
+ The computer-vision-rock-paper-scissors uses an image project model with four different classes namely: Rock, Paper, Scissors, Nothing. The rock is represented by a fist, the paper is represented by an open palm, scissors is represented by two raised fingers while nothing is represented by no symbol or action. Teachable-Machine technology is used where images for each option are taken and then trained. To increase the model accuracy, several images of each class are taken from diffenet directions. Once satisfied with the model, the model is downloaded through the 'Tensorflow' tab containing the labels.txt and keras_model.h5 files.
+
+ ## Milestone 3
+ A new virtual environment is created and appropriate libraries that work with the model are installed. The libraries rwquired for this model are opencv-python, tensorflow ad ipykernel. Once installation of the dependencies is complete, a requirements.txt file is created by running the command: pip list > requirements.txt. The requirements.txt file will enable any other person using the computer to install the exact dependences by running the command: pip install requirements.txt
+
+ ## Milestone 4
+ A new python file named manual_rps is created where the code to play the rock paper scissors game between a computer and a player is developed. As more than two functions are used to make up the code for the game, a  python class named Manual_RPS is used. The game is centred around three choices namely: rock, paper, scissors. To enable the computer to make random choices, the random module is imported into the code. After initialising the choice_list variable, the get_user_choice function is defined to prompt the player to choose a choice using the built-in 'input' function. The user input is converted to lower case to match the lower case used in the choice_list variable. Should the player's input not in the choice_list, the while loop will reject it and request a new iput, otherwise the function prints and returns the choice. The choice is stored in a variable called player_choice.
+
+ The get_computer_choice function is then defined and the random module is used to make a choice from the word_list and stored in a variable called computer_choice.This function also prints the computer choice.
+
+ To establish the winner, a get_winner function is defined. Several if-elif-else statements are created to so that a clear winner of the game is ascertained. The function also prints the outcome, 'You win', 'You lost', 'It's a tie'.
+
+ To run the whole code together, the class variable by the name play is assigned. The three instances of the class are then called.
--- manual_rps.py
+++ manual_rps.py
@@ -1,56 +1,54 @@
 import random
 choice_list = ['rock','paper','scissors']
+class ManualRockPaperScissors:
 
-def get_computer_choice():
-    computer_choice = random.choice(choice_list)
-    return computer_choice
+    def __init__(self):
+        self.choice_list = choice_list
 
-def get_player_choice():
-    user_choice = input(" Please choose rock,paper or scissors: ").lower()
-    while user_choice not in choice_list:
-        print(" Wrong input, Please try again.\n")
-        user_choice = get_player_choice()
-    return user_choice
+    def get_user_choice(self):
+        while True:
+            self.user_choice = input("Please choose either rock, paper or scissors  ").lower()
+            if self.user_choice not in self.choice_list:
+                print('Invalid choice.')
+                self.user_choice = self.get_user_choice()
+            else:
+                print(f'You have chosen {self.user_choice}')
+            return self.user_choice
 
-def get_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "Tie"
-    elif user_choice == "rock" and computer_choice == "scissors":
-        return "Player" 
-    elif user_choice == "paper" and computer_choice == "rock":
-       return "Player"
-    elif user_choice == "scissors" and computer_choice == "paper":
-       return "Player"
-    else:
-       return "Computer"
+    def get_computer_choice(self):
+        self.computer_choice = random.choice(choice_list)
+        print(f'Computer has chosen {self.computer_choice}')
+        return self.computer_choice
 
-def play():
-    player_score = 0
-    computer_score = 0
-    for attempts in range(1,2):
-        user_choice = get_player_choice()
-        computer_choice = get_computer_choice()
-        winner = get_winner(user_choice,computer_choice)
-        if winner == "Player":
-            player_score += 1
-            print (f'You chose {user_choice}, Computer chose {computer_choice}')
-            print (" You WON ! \n")
-        elif winner == "Computer":
-            computer_score += 1
-            print (f'You chose {user_choice}, Computer chose {computer_choice}')
-            print (" The computer won. \n")
-        else:
-            print (f'You both chose {user_choice}')
-            print('It\'s a tie! \n')
-        print (f"Your score was: {player_score}, The Computer's score was: {computer_score}")
-    if player_score > computer_score:
-        print ("You are the WINNER, CONGRATULATIONS! \n\n")
-    elif player_score < computer_score:
-        print ("The Computer is the WINNER. \n\n")
-    else:
-        print('It\'s a Tie!')    
+    def get_winner(self):
+        if self.user_choice == self.computer_choice:
+            print('It\'s a tie!')
+        elif self.user_choice == 'rock':
+            if self.computer_choice =='scissors':
+                print('You win!')
+            else:
+                print('You lost')
+        elif self.user_choice == 'paper':
+            if self.computer_choice == 'rock':
+                print('You won!')
+            else:
+                print('You lost')
+        elif self.user_choice == 'scissors':
+            if self.computer_choice == 'paper':
+                print('You won!')
+            else:
+                print('You lost')
 
-play()
+
+play = ManualRockPaperScissors()
+play.get_user_choice()
+play.get_computer_choice()
+play.get_winner()              
+
+
+
+
+
 
            
 
